20313c96ae0bd2c735c2e6a3205f27dbcd9ffd40
Fuhu Xia <fxia@reisystems.com>
2026-03-13 23:48:45 -0400

    Add weatherbarx cask

---
+++ Casks/weatherbarx.rb
@@ -0,0 +1,11 @@
+cask "weatherbarx" do
+  version "1.0.0"
+  sha256 "92b7972458b6faa6bcec5b1737ef460213959145ef6a3aacf8896a17fb4cc539"
+
+  url "https://github.com/XiaNetworks/WeatherBarX/releases/download/v1.0.0/WeatherBarX.zip"
+  name "WeatherBarX"
+  desc "Menu bar weather app for macOS"
+  homepage "https://github.com/XiaNetworks/WeatherBarX"
+
+  app "WeatherBarX.app"
+end
